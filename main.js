5bc261780cdc58da4551bcc1888cbbc51a6afec7
cong tin <pct.hcmut@gmail.com>
2021-04-07 22:54:20 +0700

    ý nghĩa của yield và 1 số định nghĩa

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ let arrSV = [
     {id:1,ten:'tin'}
 ]
 
-function * themSinhVien(sv){
+// khai báo hàm generator function => trả về nhiều giá trị sau nhiều lần gọi
+function * themSinhVien(sv){ 
 
-    yield [...arrSV,sv]
+    yield [...arrSV,sv] // yield giống lệnh return
 
     yield 'thêm sinh viên thành công'
    
@@ -14,12 +15,19 @@ function * themSinhVien(sv){
 function main(){
 
     let sv = {id:2,ten:'an'}
+
+    // muốn gọi hàm phải gán cho biến đối tượng iterator
     const iterator = themSinhVien(sv)
 
     console.log(iterator.next());
     console.log(iterator.next());
     console.log(iterator.next());
 
+    /**
+     * next() sẽ trả ra 2 giá trị là "value" and "done"
+     *  + value => là giá trị cần lấy ra ở yield
+     *  + done => cũng trả ra 2 giá trị "true" and "false" . "true" có nghĩa đã chạy hết yield, "false" chưa chạy hết yield
+     */
 
 }
 
